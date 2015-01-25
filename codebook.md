84027e0ac94910c0333bc53d8bcbe329bae79646
dancing-pig <chunxutang@gmail.com>
2015-01-25 16:24:32 -0500

    add files

---
+++ codebook.md
@@ -0,0 +1,27 @@
+# Code Book
+================
+
+## Introduction
+The R script in the directory is used to analyze text files from accelerometers from the Samsung Galaxy S smartphone and generate a tidy data set.
+
+## Raw Data
+1. features.txt  
+2. activity_labels.txt  
+3. train/x_train.txt  
+4. train/y_train.txt  
+5. test/x_test.txt  
+6. test/y_test.txt  
+7. train/subject_train.txt  
+8. test/subject_test.txt
+9. activity_labels.txt
+
+## Process Data
+Here, I merge the test set and the train set, and mainly focus on the mean and standard error measurements. Finally, I use some descriptive names to the data set and create a tidy data set stored in tidy_data.txt.
+
+## Result
+tidy_data.txt  
+1. subject: represent diffenrent subjects, range from 1 to 30.  
+2. label: represent labels of 6 different activities.
+3. features: represent mean or standard error of different features.
+
+
---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+# Function that reads data from multiple txt files from accelerometers
+# from the Samsung Galaxy S smartphone and generate a tidy data set.
+run_analysis <- function() {
+    ## Suppose that now the current directory is in UCI HAR Dataset.
+    
+    ## Read the text files and rename the headers.
+    activity_labels <- read.table("./activity_labels.txt")
+    activity_labels <- activity_labels$V2 ## Only keep activity names
+    
+    features <- read.table("./features.txt")
+    features <- features$V2 ## Only keep feature names
+    
+    x_test <- read.table("./test/X_test.txt")
+    y_test <- read.table("./test/Y_test.txt")
+    subject_test <- read.table("./test/subject_test.txt")
+    names(y_test) <- "label"
+    names(subject_test) <- "subject"
+    
+    x_train <- read.table("./train/X_train.txt")
+    y_train <- read.table("./train/Y_train.txt")
+    subject_train <- read.table("./train/subject_train.txt")
+    names(y_train) <- "label"
+    names(subject_train) <- "subject"
+    
+    ## Merge test data and train data
+    names(x_test) <- features
+    names(x_train) <- features
+    main_data <- rbind(x_test, x_train)
+    
+    ## Create a data set has subject and activity
+    data <- rbind(data.frame(y_test, subject_test), 
+                  data.frame(y_train, subject_train))
+    
+    ## Extract the features representing mean or standard error
+    mean_std <- grep("mean|std", names(main_data))
+    data <- cbind(data, main_data[mean_std])
+    
+    ## Modify the number of labels to corresponding activity
+    data$label <- as.factor(data$label)
+    data$label <- mapvalues(data$label, levels(data$label), levels(activity_labels))
+    
+    ## Create the final tidy data
+    tidy_data <- aggregate(data, by = list(data$subject, data$label), FUN = mean)
+    tidy_data <- tidy_data[, !names(tidy_data) %in% c("label", "subject")]
+    names(tidy_data)[1:2] <- c("subject", "label")
+    
+    ## Write the tidy data to a txt file
+    write.table(tidy_data, "tidy_data.txt", row.name = FALSE)
+}
+
